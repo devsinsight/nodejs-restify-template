d7fe595d6348bd80f076093c6205583392735df0
Jose Olivares <shoutmetal@gmail.com>
2017-04-23 10:18:28 -0500

    dist gitignore

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("./config/config");
-const inversify_container_1 = require("./config/inversify-container");
-exports.api = inversify_container_1.InversifyContainer({
-    name: config_1.settings.name
-});
-exports.api.listen(config_1.settings.port);
